9061d034af9d56de6685912d942571eac053f45a
priyanshumay <tiwarimay2002@gmail.com>
2020-12-27 11:41:38 +0530

    adding .env file

--- sw.js
+++ sw.js
@@ -1,56 +1,27 @@
-var dataCacheName = 'Gonewz_data';
-var cacheName = 'Gonewz_cache';
-var filesToCache = [
-  '/',
-  '/index.html',
-  '/assests/js/scripts.js',
-  '/assests/js/alpine.min.js',
-  '/assests/css/style.css',
-  '/assests/css/tailwind.min.css',
-  'assets/img/favicon.ico',
-];
+const cacheName = 'v1::static';
 
-self.addEventListener('install', function(e) {
-  console.log('[ServiceWorker] Install');
+self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      console.log('[ServiceWorker] Caching app shell');
-      return cache.addAll(filesToCache);
+    caches.open(cacheName).then(async cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/assests/js/scripts.js',
+        '/assests/js/alpine.min.js',
+        '/assests/css/style.css',
+        '/assests/css/tailwind.min.css',
+        'assets/img/favicon.ico',
+      ]).then(() => self.skipWaiting());
     })
   );
 });
 
-self.addEventListener('activate', function(e) {
-  console.log('[ServiceWorker] Activate');
-  e.waitUntil(
-    caches.keys().then(function(keyList) {
-      return Promise.all(keyList.map(function(key) {
-        if (key !== cacheName && key !== dataCacheName) {
-          console.log('[ServiceWorker] Removing old cache', key);
-          return caches.delete(key);
-        }
-      }));
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.open(cacheName).then(async cache => {
+      return cache.match(event.request).then(res => {
+        return res || fetch(event.request)
+      });
     })
   );
-  return self.clients.claim();
-});
-
-self.addEventListener('fetch', function(e) {
-  console.log('[Service Worker] Fetch', e.request.url);
-  var dataUrl = 'https://saurav.tech/NewsAPI/top-headlines/category/general/in.json';
-  if (e.request.url.indexOf(dataUrl) > -1) {
-    e.respondWith(
-      caches.open(dataCacheName).then(async function(cache) {
-        const response = await fetch(e.request);
-        cache.put(e.request.url, response.clone());
-        return response;
-      })
-    );
-  } else {
-    e.respondWith(
-      caches.match(e.request).then(function(response) {
-        return response || fetch(e.request);
-      })
-    );
-  }
 });
